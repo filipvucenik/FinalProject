fa5f66c0754d22c63b741828cf08fcb328cdaa05
Filip Vucenik <filip.vucenik@gmail.com>
2024-08-21 17:37:20 +0800

    debugging

--- SNP_tool/src/overlap_sources/overlap_source_factory.hpp
+++ SNP_tool/src/overlap_sources/overlap_source_factory.hpp
@@ -1,5 +1,4 @@
 #include <string>
 #include "overlap_source.hpp"
 
-
-OverlapSource * create_overlap_source(const std::string &library, const std::string &args);
+OverlapSource *create_overlap_source(const std::string &library, const std::string &args);
